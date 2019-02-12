4b5fe1a3e62d41a3f3cfd0c048ba909ce70c7e43
Lee Ralls <lee@idxbroker.com>
2019-02-12 13:16:36 -0800

    the pen is blue.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <Block tag="span">Test</Block>
+    The pen is <span class="blue">blue</span>!
   </div>
 </template>
 <script>
@@ -8,3 +9,8 @@ export default {
   name: 'app'
 }
 </script>
+<style>
+.blue {
+  color: blue;
+}
+</style>
